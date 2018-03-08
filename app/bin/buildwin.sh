67e5b73d8363020ee83482437ff86162ed50b2f5
monsterkodi <monsterkodi@gmx.net>
2018-03-08 18:02:54 +0100

    buildwin

--- app/bin/buildwin.sh
+++ app/bin/buildwin.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd `dirname $0`/..
 
+rm -f konrad-win32-x64
+
 konrad
 
 node_modules/.bin/electron-rebuild
@@ -8,6 +10,4 @@ node_modules/.bin/electron-rebuild
 IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|pug$|styl$|package\.noon$|.*\.lock$|img/dmg.*\.png)"
 
 node_modules/electron-packager/cli.js . --overwrite --icon=img/konrad.ico --no-prune --ignore=$IGNORE
-
-rm -f konrad-win32-x64/LICENSE*
-rm -f konrad-win32-x64/version
+
