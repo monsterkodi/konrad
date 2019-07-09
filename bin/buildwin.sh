820eae315096db5c7e77b9531e241fc84bd3136b
monsterkodi <monsterkodi@gmx.net>
2019-07-09 18:27:27 +0200

    misc

--- bin/buildwin.sh
+++ bin/buildwin.sh
@@ -9,7 +9,7 @@ if rm -rf konrad-win32-x64; then
     
     IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|pug$|styl$|package\.noon$|.*\.lock$|img/dmg.*\.png)"
     
-    node_modules/electron-packager/cli.js . --overwrite --icon=img/app.ico --ignore=$IGNORE
+    node_modules/.bin/electron-packager . --overwrite --icon=img/app.ico --ignore=$IGNORE
     
     ./konrad-win32-x64/konrad.exe &
 fi
