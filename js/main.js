62e870550941e95b2ac199a5e19b5e97caa93234
monsterkodi <monsterkodi@gmx.net>
2021-12-20 05:41:52 +0100

    misc

---
+++ js/main.js
@@ -0,0 +1,252 @@
+// monsterkodi/kode 0.146.0
+
+var _k_ = {in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}, empty: function (l) {return l==='' || l===null || l===undefined || l!==l || typeof(l) === 'object' && Object.keys(l).length === 0}, valid: undefined}
+
+var kxk, app, args, childp, klog, kolor, kstr, noon, post, prefs, slash, udp, pkg, electron, konrad, konradVersion, konradUdp, konradSend, p, startKonrad, createWindow, quit, setRootDir, highlight
+
+kxk = require('kxk')
+app = kxk.app
+args = kxk.args
+childp = kxk.childp
+klog = kxk.klog
+kolor = kxk.kolor
+kstr = kxk.kstr
+noon = kxk.noon
+post = kxk.post
+prefs = kxk.prefs
+slash = kxk.slash
+udp = kxk.udp
+
+pkg = require('../package.json')
+electron = require('electron')
+app = new app({dir:__dirname,pkg:pkg,shortcut:'CmdOrCtrl+F2',index:'index.html',icon:'../img/app.ico',tray:'../img/menu@2x.png',about:'../img/about.png',onQuit:function ()
+{
+    return quit()
+},width:400,height:400,minWidth:300,minHeight:200,args:`show      open window on startup  true
+prefs     show preferences        false`})
+konrad = null
+konradVersion = null
+konradUdp = new udp({port:9559})
+
+konradSend = function (msg, ...args)
+{
+    var s
+
+    s = [msg + ':'].concat(args).join(' ')
+    return konradUdp.send(s)
+}
+if (args.verbose)
+{
+    klog(kolor.white(kolor.bold(`\n${pkg.name}`,kolor.gray(`v${pkg.version}\n`))))
+    klog(kolor.yellow(kolor.bold('process')))
+    p = {cwd:process.cwd()}
+    klog(noon.stringify(p,{colors:true}))
+    klog(kolor.yellow.bold('args'))
+    klog(noon.stringify(args,{colors:true}))
+    klog('')
+}
+if (args.prefs)
+{
+    klog(kolor.yellow(kolor.bold('prefs')))
+    klog(kolor.green(kolor.bold(prefs.store.file)))
+    if (slash.fileExists(prefs.store.file))
+    {
+        klog(noon.stringify(noon.load(prefs.store.file),{colors:true}))
+    }
+}
+post.on('Restart konrad',function ()
+{
+    return startKonrad(prefs.get('rootDir'))
+})
+
+startKonrad = function (rootDir)
+{
+    var treekill, path
+
+    prefs.set('rootDir',rootDir)
+    if ((konrad != null))
+    {
+        console.log('killing konrad',konrad.pid)
+        treekill = require('tree-kill')
+        treekill(konrad.pid)
+    }
+    path = slash.resolve(`${__dirname}/../js/konrad.js`)
+    if (!(_k_.in('/usr/local/bin',process.env.PATH.split(':'))))
+    {
+        process.env.PATH = process.env.PATH + ':/usr/local/bin'
+    }
+    if (!(_k_.in('/opt/homebrew/bin',process.env.PATH.split(':'))))
+    {
+        process.env.PATH = process.env.PATH + ':/opt/homebrew/bin'
+    }
+    konrad = childp.spawn('node',['--trace-warnings',path,'-w','-v'],{cwd:rootDir,shell:true,env:process.env,detached:false})
+    konrad.on('exit',function (code, signal)
+    {
+        console.log('konrad.on exit',code,signal)
+        return konradSend('exit')
+    })
+    konrad.on('close',function (code, signal)
+    {
+        post.toWins('konradExit',`konrad exit code: ${code}`)
+        return konradSend('exit')
+    })
+    konrad.stderr.on('data',function (data)
+    {
+        var s, _105_19_
+
+        s = kstr.stripAnsi(data.toString())
+        konradSend('error',s)
+        if (((app != null ? app.win : undefined) != null))
+        {
+            return post.toWins('konradError',`konrad error: ${s}`,kstr.ansi2html(data.toString()))
+        }
+        else
+        {
+            return createWindow('konradError',`konrad error: ${s}`,kstr.ansi2html(data.toString()))
+        }
+    })
+    return konrad.stdout.on('data',function (data)
+    {
+        var s, _118_23_
+
+        s = kstr.stripAnsi(data.toString())
+        if (/\ 👁\ \ /.test(s))
+        {
+            konradVersion = s.split('👁  ')[1]
+            post.toWins('konradVersion',konradVersion)
+            return konradSend('version',konradVersion)
+        }
+        else if ((app.win != null))
+        {
+            if (/ 😡 /.test(s))
+            {
+                post.toWins('konradError',s,kstr.ansi2html(data.toString()))
+                konradSend('error',s)
+                app.win.show()
+                return app.win.focus()
+            }
+            else
+            {
+                post.toWins('konradOutput',s,kstr.ansi2html(data.toString()))
+                return konradSend('output',s)
+            }
+        }
+        else
+        {
+            if (/ 😡 /.test(s))
+            {
+                konradSend('error',s)
+                return createWindow('konradError',s,kstr.ansi2html(data.toString()))
+            }
+            else
+            {
+                konradSend('output',s)
+                return highlight()
+            }
+        }
+    })
+}
+
+createWindow = function (msg, s, h)
+{
+    return app.createWindow(function (w)
+    {
+        return post.toWin(w.id,msg,s,h)
+    })
+}
+
+quit = function ()
+{
+    var treekill
+
+    if ((konrad != null))
+    {
+        klog('killing konrad',(konrad != null ? konrad.pid : undefined))
+        treekill = require('tree-kill')
+        treekill(konrad.pid,function ()
+        {
+            return app.exitApp()
+        })
+        konrad = null
+        return 'delay'
+    }
+}
+post.on('Restart',function ()
+{
+    var treekill
+
+    klog('on Restart',konrad.pid)
+    treekill = require('tree-kill')
+    return treekill(konrad.pid,function ()
+    {
+        klog('spawn',process.argv[0],process.argv.slice(1))
+        childp.spawn(process.argv[0],process.argv.slice(1),{cwd:process.cwd(),encoding:'utf8',detached:true,shell:true,windowsHide:true})
+        return process.exit(0)
+    })
+})
+
+setRootDir = function ()
+{
+    var opts
+
+    opts = {title:'Open',properties:['openDirectory']}
+    return electron.dialog.showOpenDialog(opts).then((function (result)
+    {
+        if (!result.canceled && !_k_.empty(result.filePaths))
+        {
+            return startKonrad(result.filePaths[0])
+        }
+    }).bind(this))
+}
+post.on('setRootDir',setRootDir)
+post.on('appReady',function ()
+{
+    var rootDir
+
+    if (rootDir = prefs.get('rootDir'))
+    {
+        return startKonrad(rootDir)
+    }
+    else
+    {
+        return setRootDir()
+    }
+})
+post.on('winReady',function (wID)
+{
+    if (konradVersion)
+    {
+        return post.toWin(wID,'konradVersion',konradVersion)
+    }
+})
+
+highlight = function ()
+{
+    var _220_26_, unhighlight, _228_33_
+
+    if (!(app.tray != null))
+    {
+        return
+    }
+    if (slash.win())
+    {
+        app.tray.setImage(slash.resolve(slash.join(__dirname,'../img/menu.png')))
+        unhighlight = function ()
+        {
+            return app.tray.setImage(slash.resolve(slash.join(__dirname,'../img/menu@2x.png')))
+        }
+        return setTimeout(unhighlight,1000)
+    }
+    else
+    {
+        (typeof app.tray.setHighlightMode === "function" ? app.tray.setHighlightMode('always') : undefined)
+        unhighlight = function ()
+        {
+            var _229_50_
+
+            return (typeof app.tray.setHighlightMode === "function" ? app.tray.setHighlightMode('never') : undefined)
+        }
+        return setTimeout(unhighlight,1000)
+    }
+}
+post.on('highlight',highlight)
---
+++ js/window.js
@@ -0,0 +1,246 @@
+// monsterkodi/kode 0.146.0
+
+var _k_ = {in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}}
+
+var $, _, elem, post, slash, title, udp, win, w, koSend, openFile, tasks, showOverlay, fadeOverlay, taskDiv, onTask, onMessage, onError, onFile
+
+$ = require('kxk').$
+_ = require('kxk')._
+elem = require('kxk').elem
+post = require('kxk').post
+slash = require('kxk').slash
+title = require('kxk').title
+udp = require('kxk').udp
+win = require('kxk').win
+
+w = new win({dir:__dirname,pkg:require('../package.json'),menu:'../kode/menu.noon',icon:'../img/menu@2x.png'})
+koSend = null
+
+openFile = function (f)
+{
+    if (!koSend)
+    {
+        koSend = new udp({port:9779})
+    }
+    return koSend.send(slash.resolve(f))
+}
+tasks = {}
+
+showOverlay = function ()
+{
+    var img, _39_17_, overlay
+
+    img = slash.fileUrl(slash.join(__dirname,'..','img','about.png'))
+    ;($('#overlay') != null ? $('#overlay').remove() : undefined)
+    overlay = elem({id:'overlay'})
+    overlay.appendChild(elem('img',{class:'info',src:img}))
+    overlay.addEventListener('click',function (event)
+    {
+        return event.target.remove()
+    })
+    return $('main').appendChild(overlay)
+}
+
+fadeOverlay = function ()
+{
+    showOverlay()
+    return $('#overlay').classList.add('fade-in')
+}
+post.on('konradExit',function (s)
+{})
+post.on('konradError',function (s, html)
+{
+    return onError(s,html)
+})
+post.on('konradOutput',function (s, html)
+{
+    if (/ 😡 /.test(s))
+    {
+        onError(s,html)
+    }
+    if (/ 🔺 /.test(s))
+    {
+        return onFile(s,html)
+    }
+    else if (/ 👍 /.test(s))
+    {
+        return onTask(s,html)
+    }
+    else if (/ 🔧 /.test(s))
+    {
+        return onMessage(s,html)
+    }
+})
+post.on('konradVersion',function (s)
+{
+    var split
+
+    split = s.trim().split(/\s+/)
+    title = ['path']
+    if (process.argv[0].endsWith('Electron Helper') || process.argv[0].endsWith('electron.exe'))
+    {
+        title = ['version','path']
+    }
+    return window.titlebar.setTitle({title:title,pkg:{version:split[0],path:slash.tilde(split[2])}})
+})
+post.on('clearLog',function ()
+{
+    $('main').innerHTML = ''
+    tasks = {}
+    return showOverlay()
+})
+
+taskDiv = function (opt)
+{
+    var main, div, tim, fil, _108_30_, _109_33_, _1_9_
+
+    main = $('main')
+    if (_.isEmpty(tasks))
+    {
+        main.innerHTML = ''
+    }
+    ;(tasks[opt.key] != null ? tasks[opt.key].remove() : undefined)
+    div = document.createElement('div')
+    tim = document.createElement('span')
+    fil = document.createElement('span')
+    div.classList.add('task')
+    tim.classList.add('time')
+    tim.innerHTML = opt.time
+    fil.classList.add((opt.file != null) && 'file' || 'message')
+    fil.innerHTML = ((_109_33_=opt.fileHtml) != null ? _109_33_ : ` ${opt.icon} ${((_1_9_=opt.file) != null ? _1_9_ : opt.message)}`)
+    fil.onclick = function ()
+    {
+        var _110_50_
+
+        if ((opt.file != null))
+        {
+            return openFile(opt.file)
+        }
+    }
+    div.appendChild(tim)
+    div.appendChild(fil)
+    tasks[opt.key] = div
+    main.appendChild(div)
+    return div
+}
+
+onTask = function (s)
+{
+    var time, sourceTarget, source, target, div
+
+    post.toMain('highlight')
+    var _130_25_ = s.split(' 👍 ') ; time = _130_25_[0]    ; sourceTarget = _130_25_[1]
+
+    var _131_21_ = sourceTarget.split(' ► ') ; source = _131_21_[0]    ; target = _131_21_[1]
+
+    source = slash.tilde(source.trim())
+    target = slash.tilde(target.trim())
+    div = taskDiv({time:time,file:source,key:source,icon:'👍'})
+    div.scrollIntoViewIfNeeded()
+    if (slash.dir(target).startsWith(slash.tilde(slash.dir(__filename))))
+    {
+        if (slash.file(target) === 'window.js' || _k_.in(slash.ext(target),['css','html']))
+        {
+            post.emit('menuAction','Reload')
+        }
+        else if (slash.file(target) === 'main.js')
+        {
+            post.toMain('Restart')
+        }
+        else
+        {
+            post.toMain('Restart konrad')
+        }
+    }
+    return fadeOverlay()
+}
+
+onMessage = function (s)
+{
+    var time, msg, div
+
+    var _157_16_ = s.split(' 🔧 ') ; time = _157_16_[0]    ; msg = _157_16_[1]
+
+    return div = taskDiv({time:time,message:msg,key:'msg',icon:'🔧'})
+}
+
+onError = function (s, html)
+{
+    var key, task, lines, htmls, fileHtml, time, msg, _183_16_, div, i, pre
+
+    post.toMain('showWindow')
+    post.toMain('highlight')
+    for (key in tasks)
+    {
+        task = tasks[key]
+        if (task.icon === '😡')
+        {
+            task.div.remove()
+            delete tasks[key]
+        }
+    }
+    lines = s.split('\n')
+    if (html)
+    {
+        htmls = html.split('\n')
+        fileHtml = htmls.shift().split('</span>').slice(5).join('</span>')
+    }
+    var _182_16_ = lines.shift().split(' 😡 ') ; time = _182_16_[0]    ; msg = _182_16_[1]
+
+    if (((msg != null ? msg.trim : undefined) != null))
+    {
+        msg = msg.trim()
+        if (msg.split(':').length >= 3)
+        {
+            msg = ''
+        }
+    }
+    div = taskDiv({time:time,icon:'😡',message:msg})
+    for (i = 0; i < lines.length; i++)
+    {
+        pre = document.createElement('pre')
+        pre.classList.add('error')
+        if (htmls[i])
+        {
+            pre.innerHTML = htmls[i]
+        }
+        else
+        {
+            pre.textContent = lines[i]
+        }
+        div.appendChild(pre)
+    }
+    return div.scrollIntoViewIfNeeded()
+}
+
+onFile = function (s, html)
+{
+    var htmls, fileHtml, time, file, div
+
+    post.toMain('showWindow')
+    post.toMain('highlight')
+    if (html)
+    {
+        htmls = html.split('\n')
+        fileHtml = htmls.shift().split('</span>').slice(5).join('</span>')
+    }
+    var _215_17_ = s.split(' 🔺 ') ; time = _215_17_[0]    ; file = _215_17_[1]
+
+    file = file.trim()
+    div = taskDiv({time:time,file:file,key:file.split(':')[0],icon:'🔺',fileHtml:fileHtml})
+    return div.scrollIntoViewIfNeeded()
+}
+post.on('menuAction',function (action)
+{
+    switch (action)
+    {
+        case 'Clear':
+            return post.emit('clearLog')
+
+        case 'Set Dir...':
+            return post.toMain('setRootDir')
+
+    }
+
+})
+showOverlay()
